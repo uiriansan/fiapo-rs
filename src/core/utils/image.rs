4bea74e3e6da8acfeb036ca400dc230f823fadfb
uiriansan <uiriansan@gmail.com>
2025-07-15 20:22:23 -0300

    Convert DynamicImage to Gtk.Image

--- src/core/utils/image.rs
+++ src/core/utils/image.rs
@@ -1,7 +1,44 @@
-use image::ImageReader;
+use gtk4::glib::object::Cast;
+use image::{DynamicImage, ImageReader};
+
+pub fn open_image(image: &str) -> Result<DynamicImage, Box<dyn std::error::Error>> {
+    let img = ImageReader::open(image)?.decode()?;
+    Ok(img)
+}
+
+pub fn dynamic_image_to_pixbuf(
+    img: &DynamicImage,
+) -> Result<gtk4::gdk_pixbuf::Pixbuf, gtk4::glib::Error> {
+    let rgba_img = img.to_rgba8();
+    let (width, height) = rgba_img.dimensions();
+    let bytes = rgba_img.into_raw();
+    let pixbuf = gtk4::gdk_pixbuf::Pixbuf::from_bytes(
+        &gtk4::glib::Bytes::from(&bytes),
+        gtk4::gdk_pixbuf::Colorspace::Rgb,
+        true, // alpha
+        8,    // bits pp
+        width as i32,
+        height as i32,
+        (width * 4) as i32,
+    );
+    Ok(pixbuf)
+}
+
+pub fn dynamic_image_to_texture(
+    img: &DynamicImage,
+) -> Result<gtk4::gdk::Texture, gtk4::glib::Error> {
+    let pixbuf = dynamic_image_to_pixbuf(&img).unwrap();
+    let texture = gtk4::gdk::Texture::for_pixbuf(&pixbuf);
+    Ok(texture)
+}
+
+pub fn get_texture_from_image(img: gtk4::Image) -> gtk4::gdk::Texture {
+    let paintable = img.paintable();
+    return paintable.unwrap().downcast::<gtk4::gdk::Texture>().unwrap();
+}
 
 pub fn invert_image() -> Result<(), Box<dyn std::error::Error>> {
-    let mut img = ImageReader::open("assets/teapot.png")?.decode()?;
+    let mut img = open_image("assets/teapot.png").unwrap();
     img.invert();
     img.save("assets/invteapot.png")?;
 
--- src/core/window.rs
+++ src/core/window.rs
@@ -1,8 +1,12 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Box, Image};
 use gtk4 as gtk;
 
 use crate::core::components::icon_button;
+use crate::core::utils::image;
 
 pub fn create(app: Application) {
     let window = ApplicationWindow::builder()
@@ -12,10 +16,32 @@ pub fn create(app: Application) {
         .default_height(600)
         .build();
 
+    let container = Box::new(gtk::Orientation::Vertical, 10);
+    container.set_margin_top(300);
+
+    let img = image::open_image("assets/teapot.png").unwrap();
+    let texture = image::dynamic_image_to_texture(&img);
+
+    let image = Image::from_paintable(Some(&texture.unwrap()));
+    image.set_size_request(200, 200);
+    image.set_margin_top(50);
+
+    let img_rc = Rc::new(RefCell::new(img));
+    let cloned_img = img_rc.clone();
+    let cloned_gtk_img = image.clone();
+
     let button = icon_button::create("Click me!!!");
-    button.connect_clicked(|_| {
-        eprintln!("Clicked!");
+    button.connect_clicked(move |_| {
+        let mut img = cloned_img.borrow_mut();
+        img.invert();
+        let texture = image::dynamic_image_to_texture(&img);
+        cloned_gtk_img.set_paintable(Some(&texture.unwrap()));
     });
-    window.set_child(Some(&button));
+    button.set_cursor(gtk4::gdk::Cursor::from_name("pointer", None).as_ref());
+
+    container.append(&button);
+    container.append(&image);
+
+    window.set_child(Some(&container));
     window.present();
 }
